a387d2c77ed54ac21c50df62075f087a00366b11
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-09-03 16:52:21 -0300

    feat: render Information and clipboard

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //@ts-ignore
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
+type Article = {
+  url: string;
+  summary: string;
+};
 export function Demo() {
-  const [article, setArticle] = useState({
+  const [article, setArticle] = useState<Article>({
     url: "",
     summary: "",
   });
+  const [allArticles, setAllArticles] = useState<Article[]>([]);
+
+  const [copied, setCopied] = useState();
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -18,8 +25,13 @@ export function Demo() {
     });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
       console.log(newArticle);
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
   const handleChangeArticleUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,6 +41,23 @@ export function Demo() {
     });
   };
 
+  const handleCopy = (copyUrl) => {
+    navigator.clipboard.writeText(copyUrl);
+    setCopied(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  useEffect(() => {
+    const articles = localStorage.getItem("articles");
+    if (!articles) {
+      return;
+    }
+    const articlesFromLocalStorage = JSON.parse(articles);
+    if (!articlesFromLocalStorage) {
+      return;
+    }
+    setAllArticles(articlesFromLocalStorage);
+  }, []);
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
@@ -60,8 +89,60 @@ export function Demo() {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              className="link_card"
+              onClick={() => {
+                setArticle(item);
+              }}
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img
+            src={loader}
+            alt="loader"
+            className="w-20 h-20 object-contain animate-spin"
+          />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 }
